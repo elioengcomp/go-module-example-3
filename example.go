1b8b3c7ec8fbe9ff442c371772ac7b9eee4ed4dd
Elio Marcolino <eliom@jfrog.com>
2018-11-02 16:19:35 -0700

    Added not converted dependency

--- example.go
+++ example.go
@@ -3,6 +3,7 @@ package example
 import (
 	"fmt"
 
+	example_4 "github.com/elioengcomp/go-module-example-4"
 	example_v3 "github.com/elioengcomp/go-module-example/v3"
 )
 
@@ -10,5 +11,6 @@ const version = "v1.0.0"
 
 func Exec() string {
 
-	return fmt.Sprintf("This is go-module-example-3 %s consuming go-module-example/v3: %s", version, example_v3.Exec())
+	return fmt.Sprintf("This is go-module-example-3 %s consuming go-module-example/v3: %s and go-module-example-4: %s",
+		version, example_v3.Exec(), example_4.Exec())
 }
